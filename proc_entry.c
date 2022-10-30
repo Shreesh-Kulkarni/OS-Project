15180aac84b5217cb3eeaeeef4a568b8ecd6045f
Shreesh Kulkarni <kshreesh5@gmail.com>
2022-10-30 23:28:33 +0530

    code for module instantiation

---
+++ proc_entry.c
@@ -0,0 +1,26 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/proc fs.h>
+#include <asm/uaccess.h>
+#define BUFFER SIZE 128
+#define PROC NAME "YourProcNameHere"
+ssize t proc read(struct file *file, char user *usr buf,
+size t count, loff t *pos);
+static struct file operations proc ops = {
+.owner = THIS MODULE,
+.read = proc read,
+};
+/* This function is called when the module is loaded. */
+int proc init(void)
+{
+/* creates the /proc/YourProcNameHere entry */
+proc create(PROC NAME, 0666, NULL, &proc ops);
+return 0;
+}
+/* This function is called when the module is removed. */
+void proc exit(void)
+{
+/* removes the /proc/YourProcNameHere entry */
+remove proc entry(PROC NAME, NULL);
+}
