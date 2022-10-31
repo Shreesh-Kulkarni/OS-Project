2a0692d2faffd26d426aea82db70e8d0e5376163
Shreesh Kulkarni <kshreesh5@gmail.com>
2022-10-31 18:43:56 +0530

    Update proc_read.c

--- proc_read.c
+++ proc_read.c
@@ -1,22 +1,16 @@
-/* This function is called each time /proc/YourProcNameHere is read */
-ssize t proc read(struct file *file, char user *usr buf,
-size t count, loff t *pos)
-{
-int rv = 0;
-char buffer[BUFFER SIZE];
-static int completed = 0;
-if (completed) {
-completed = 0;
-return 0;
+//Function to read the pid value and print it
+
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos) {
+	// ...
+    struct task_struct *tsk = NULL;
+    tsk = pid_task(find_vpid(current_pid), PIDTYPE_PID);
+    if(tsk) {
+        rv = snprintf(buffer, BUFFER_SIZE,
+                      "command = [%s], pid = [%d], state = [%ld]\n",
+                      tsk->comm, current_pid, tsk->state);
+    } else {
+        printk(KERN_INFO "Invalid PID %d!", current_pid);
+        return 0;
+    }
+	// ...
 }
-completed = 1;
-rv = sprintf(buffer, "This is the proc read function");
-/* copies kernel space buffer to user space usr buf */
-copy to user(usr buf, buffer, rv);
-return rv;
-}
-module init(proc init);
-module exit(proc exit);
-MODULE LICENSE("GPL");
-MODULE DESCRIPTION("PROC MOD");
-MODULE AUTHOR("SGG");
